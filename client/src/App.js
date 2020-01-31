3deb70c154d9b23e2e015faa05ece9ada7239214
Shawn Pereira <recurshawn@users.noreply.github.com>
2020-01-31 23:31:00 +0530

    Added line chart

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Chart } from "react-google-charts";
 
 import './App.css';
 
@@ -6,40 +7,62 @@ class App extends React.Component {
   state = {
     unit: 'week',
   }
+
+
   render() {
     return (
       <div className="App container">
         <h4 className="summary alert alert-success">Looks like someone's {this.state.unit} was awesome!</h4>
-        <br/>
+        <br />
         <h4>Let's look at your mood for the {this.state.unit}</h4>
-        <div className="temp">
-        { //FOR SHAWN
-          //Insert line graph of happiness wrt day here
-        }
+        <div className="mood-chart">
+          <Chart
+            chartType="LineChart"
+            data={[["Day of the Week", "Happiness Levels"], [new Date(2020, 1, 1), 5], [new Date(2020, 1, 2), -3], [new Date(2020, 1, 3), -7], [new Date(2020, 1, 4), 5], [new Date(2020, 1, 5), -1], [new Date(2020, 1, 6), 0], [new Date(2020, 1, 7), 5]]}
+            width="100%"
+            height="400px"
+            options={{
+              hAxis: {
+                title: '',
+                
+              },
+              vAxis: {
+                title: '',
+                textPosition: 'none',
+              },
+              series: {
+                0: { curveType: 'function' },
+              },
+            }}
+          />
         </div>
-        <br/>
+        <br />
         <h4>Here's what we think causes your moods</h4>
         <div className="temp">
-        { //FOR KUNAL
-          // Make a table with mood images on one side and causes on the other
-        }
+          { //FOR KUNAL
+            // Make a table with mood images on one side and causes on the other
+          }
         </div>
-        <br/>
+        <br />
         <h4>We noticed that you care about these a lot</h4>
         <div className="temp">
           { //FOR SHAWN
             //Insert bubble graph of desires here
           }
+
         </div>
         <p>Introspect on these desires. In our experience, we notice that happiness levels are strongly related to your desires.</p>
         <p>People with fewer desires tend to be happier - eliminate desires by meeting them or deciding on what's truly important.</p>
         <h4>Congratulations! Here's what you did this week</h4>
         <div className="temp">
-        { //FOR SHAWN FIRST
-          //THEN FOR KUNAL
-          //Insert list of accomplishments
-        }
+          { //FOR SHAWN FIRST
+            //THEN FOR KUNAL
+            //Insert list of accomplishments
+          }
         </div>
+        {
+
+        }
       </div>
     );
   }
